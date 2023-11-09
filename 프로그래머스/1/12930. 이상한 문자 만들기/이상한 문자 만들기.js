418bce7b5768d725af956c37a8a402cee449af4b
ay0530 <54698039+ay0530@users.noreply.github.com>
2023-11-09 15:27:58 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -7,9 +7,7 @@ function solution(s) {
         for (let j = 0; j < word.length; j++) {
             j % 2 === 0 ? answer.push(arr[i][j].toUpperCase()) : answer.push(arr[i][j].toLowerCase());
         };
-        if (i < arr.length - 1) { answer.push(' '); };
     };
 
-    answer = answer.join('');
     return answer;
 }
